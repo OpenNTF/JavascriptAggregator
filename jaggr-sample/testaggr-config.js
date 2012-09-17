79ef6ff77e1a4fca25dfd5a768c4423096bf088b
Dan Dumont <ddumont@us.ibm.com>
2012-09-17 14:32:25 -0400

    Update licenses im prep for first commit. Maven will now verify licenses on all files in the projects during build.

--- jaggr-sample/testaggr-config.js
+++ jaggr-sample/testaggr-config.js
@@ -1,12 +1,23 @@
 /*
- * Copyright (c) 2004-2012, The Dojo Foundation All Rights Reserved.
- * Available via Academic Free License >= 2.1 OR the modified BSD license.
- * see: http://dojotoolkit.org/license for details
+ * (C) Copyright 2012, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-// This file is AMD Aggregator config for the Aggregator sample application 
+
+// This file is AMD Aggregator config for the Aggregator sample application
 {
 	baseUrl: 'WebContent',
-	
+
 	packages: [
 		{
 			name: 'dojo',
@@ -22,13 +33,13 @@
 		}
 	],
 	paths: {
-		'dojo/dojo' : 'dojo-patched/dojo'	// http://bugs.dojotoolkit.org/ticket/14198 
+		'dojo/dojo' : 'dojo-patched/dojo'	// http://bugs.dojotoolkit.org/ticket/14198
 	},
-	
+
 	depsIncludeBaseUrl: true,		// include baseUrl in scan for dependencies
-	
+
 	inlinedImageSizeThreshold: 1024,	// max image size to inline in CSS
-	
+
 	notice: 'notice.txt'
 
 }
