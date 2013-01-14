06dce98f9f93ff4945b3d7aab0ef1ad19a15398d
chuckd <chuckd@us.ibm.com>
2013-01-14 11:36:04 -0500

    Fix for URLConverter.toFileURL() issue on some platforms

--- jaggr-service/src/main/java/com/ibm/jaggr/service/impl/config/ConfigImpl.java
+++ jaggr-service/src/main/java/com/ibm/jaggr/service/impl/config/ConfigImpl.java
@@ -596,8 +596,7 @@ public class ConfigImpl implements IConfig, IShutdownListener, IOptionsListener 
 			// Use newResource to convert the bundleresource uri into a file uri if the
 			// platform supports it.  File uri's are more stable across server restarts
 			// than bundleresource uri's.
-			URL bundleRoot = getAggregator().getBundleContext().getBundle().getResource("/");
-			configUri = aggregator.newResource(PathUtil.url2uri(bundleRoot)).getURI().resolve(configName);
+			configUri = aggregator.newResource(PathUtil.url2uri(configUrl)).getURI();
 		}
 		return configUri;
 	}
