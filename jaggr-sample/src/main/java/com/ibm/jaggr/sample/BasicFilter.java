510f0d4fd95c23a767ff4394b96a2b90118c0ec9
Dan Dumont <ddumont@us.ibm.com>
2013-03-05 16:45:33 -0500

    equinox.http.registy does not handle pax-web HttpService impl.

--- jaggr-sample/src/main/java/com/ibm/jaggr/sample/BasicFilter.java
+++ jaggr-sample/src/main/java/com/ibm/jaggr/sample/BasicFilter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
@@ -27,25 +28,27 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-public class Filter implements javax.servlet.Filter {
-	private static final String CLASS_NAME = Filter.class.getName();
-	private static final Logger LOGGER = Logger.getLogger(CLASS_NAME);
+public class BasicFilter implements Filter {
+
+	private static final String CLAZZ = BasicFilter.class.getName();
+	private static final Logger LOGGER = Logger.getLogger(CLAZZ);
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		LOGGER.logp(Level.WARNING, CLASS_NAME, "init", "INIT CALLED.");
+		LOGGER.logp(Level.WARNING, CLAZZ, "init", "INIT CALLED.");
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		LOGGER.logp(Level.WARNING, CLASS_NAME, "doFilter", "DOFILTER CALLED.");
+		LOGGER.logp(Level.WARNING, CLAZZ, "doFilter", "DOFILTER CALLED.");
 		HttpServletResponse resp = (HttpServletResponse)response;
 		resp.setHeader("X-TEST-HEADER", "TEST");
+		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void destroy() {
-		LOGGER.logp(Level.WARNING, CLASS_NAME, "destroy", "DESTROY CALLED.");
+		LOGGER.logp(Level.WARNING, CLAZZ, "destroy", "DESTROY CALLED.");
 	}
 
 }
