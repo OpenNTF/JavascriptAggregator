ed32b93aa8663bd0514653c6f47d2dfe4e3e0bab
chuckd <chuckd@us.ibm.com>
2013-03-12 18:02:36 -0400

    Remove apache license from MIT licensed code

--- jaggr-service/src/main/java/com/ibm/jaggr/service/impl/deps/Term.java
+++ jaggr-service/src/main/java/com/ibm/jaggr/service/impl/deps/Term.java
@@ -1,19 +1,3 @@
-/*
- * (C) Copyright 2012, IBM Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /* Copyright (c) 2013 the authors listed at the following URL, and/or
 the authors of referenced articles or incorporated external code:
 http://en.literateprograms.org/Quine-McCluskey_algorithm_(Java)?action=history&offset=20110925122251
