e73bc1317f62636add06b8f9238def844f50efa3
Dan Dumont <ddumont@us.ibm.com>
2014-01-23 16:25:36 -0500

    Move c.i.j.service.impl.config message bundle to c.i.j.core.config

--- jaggr-core/src/main/java/com/ibm/jaggr/core/impl/config/Messages.java
+++ jaggr-core/src/main/java/com/ibm/jaggr/core/impl/config/Messages.java
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-package com.ibm.jaggr.service.impl.config;
+package com.ibm.jaggr.core.impl.config;
 
-import org.eclipse.osgi.util.NLS;
+import com.ibm.jaggr.core.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "com.ibm.jaggr.service.impl.config.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = Messages.class.getPackage().getName() + ".messages"; //$NON-NLS-1$
 	public static String ConfigImpl_1;
 	public static String ConfigImpl_2;
 	public static String ConfigImpl_3;
