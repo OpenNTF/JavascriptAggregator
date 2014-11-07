45ca31ee1377a274f1929d66587bf7620ab63a44
chuckd <chuckd@us.ibm.com>
2014-11-07 09:23:46 -0500

    Revert "com.ibm.jaggr.core.impl.cache.AggregatorProxy using wrong classloader causing issues when trying to omit jaggr-core from Require-Bundles"
    This reverts commit 0a3d40a4a6d804c7d28edec705548952ebf120b6.

--- jaggr-core/src/main/java/com/ibm/jaggr/core/impl/cache/AggregatorProxy.java
+++ jaggr-core/src/main/java/com/ibm/jaggr/core/impl/cache/AggregatorProxy.java
@@ -36,7 +36,7 @@ public class AggregatorProxy implements java.lang.reflect.InvocationHandler {
 
 	public static IAggregator newInstance(IAggregator aggr, ICacheManager cacheMgr) {
 		return (IAggregator)java.lang.reflect.Proxy.newProxyInstance(
-				cacheMgr.getClass().getClassLoader(),
+				aggr.getClass().getClassLoader(),
 				new Class[]{IAggregator.class},
 				new AggregatorProxy(aggr, cacheMgr));
 	}
