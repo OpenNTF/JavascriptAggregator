f49304b8a032f78a7ecfac5cdb9f0080e86e9644
chuckd <chuckd@us.ibm.com>
2015-02-16 15:40:07 -0500

    Fix indentation.  Replace used for indentation with tabs.  No code changes.  Whitespace changes only

--- jaggr-core/src/main/java/com/ibm/jaggr/core/util/Formula.java
+++ jaggr-core/src/main/java/com/ibm/jaggr/core/util/Formula.java
@@ -36,164 +36,169 @@ import java.util.Set;
 
 
 class Formula {
-    public Formula(List<Term> termList) {
-        this.termList = termList;
-    }
-
-    public String toString() {
-        String result = ""; //$NON-NLS-1$
-        result += termList.size() + " terms, " +  //$NON-NLS-1$
-        		termList.get(0).getNumVars() + " variables\n"; //$NON-NLS-1$
-        for(int i=0; i<termList.size(); i++) {
-            result += termList.get(i) + "\n"; //$NON-NLS-1$
-        }
-        return result;
-    }
-    @SuppressWarnings("unchecked")
-    public void reduceToPrimeImplicants() {
-        originalTermList = new ArrayList<Term>(termList);
-        int numVars = termList.get(0).getNumVars();
-	ArrayList<Term>[][] table = new ArrayList[numVars + 1][numVars + 1];
-	for(int dontKnows=0; dontKnows <= numVars; dontKnows++) {
-	    for(int ones=0; ones <= numVars; ones++) {
-	        table[dontKnows][ones] = new ArrayList<Term>();
-	    }
+	public Formula(List<Term> termList) {
+		this.termList = termList;
 	}
-	for(int i=0; i<termList.size(); i++) {
-	    int dontCares = termList.get(i).countValues(Term.DontCare);
-	    int ones      = termList.get(i).countValues((byte)1);
-	    table[dontCares][ones].add(termList.get(i));
+
+	public String toString() {
+		String result = ""; //$NON-NLS-1$
+		result += termList.size() + " terms, " +  //$NON-NLS-1$
+				termList.get(0).getNumVars() + " variables\n"; //$NON-NLS-1$
+		for(int i=0; i<termList.size(); i++) {
+			result += termList.get(i) + "\n"; //$NON-NLS-1$
+		}
+		return result;
 	}
-        for(int dontKnows=0; dontKnows <= numVars - 1; dontKnows++) {
-            // Use LinkedHashSet for output lists and copy final result to
-            // ArrayList so that we have more efficient operations involving
-            // element identity (contains, removes) while building the list.
-            Set<Term> terms = new LinkedHashSet<Term>(termList);
-	    for(int ones=0; ones <= numVars - 1; ones++) {
-	        ArrayList<Term> left   = table[dontKnows][ones];
-	        ArrayList<Term> right  = table[dontKnows][ones + 1];
-	        Set<Term> out    = new LinkedHashSet<Term>();
-	        for(int leftIdx = 0; leftIdx < left.size(); leftIdx++) {
-	            for(int rightIdx = 0; rightIdx < right.size(); rightIdx++) {
-	                Term combined = left.get(leftIdx).combine(right.get(rightIdx));
-	                if (combined != null) {
-	                    if (!out.contains(combined)) {
-	                        out.add(combined);
-	                    }
-	                    terms.remove(left.get(leftIdx));
-			    terms.remove(right.get(rightIdx));
-			    if (!terms.contains(combined)) {
-			        terms.add(combined);
-			    }
-	                }
-	            }
-	        }
-	        table[dontKnows+1][ones] = new ArrayList<Term>(out);
-	    }
-	    termList = new ArrayList<Term>(terms);
+
+	@SuppressWarnings("unchecked")
+	public void reduceToPrimeImplicants() {
+		originalTermList = new ArrayList<Term>(termList);
+		int numVars = termList.get(0).getNumVars();
+		ArrayList<Term>[][] table = new ArrayList[numVars + 1][numVars + 1];
+		for(int dontKnows=0; dontKnows <= numVars; dontKnows++) {
+			for(int ones=0; ones <= numVars; ones++) {
+				table[dontKnows][ones] = new ArrayList<Term>();
+			}
+		}
+		for(int i=0; i<termList.size(); i++) {
+			int dontCares = termList.get(i).countValues(Term.DontCare);
+			int ones      = termList.get(i).countValues((byte)1);
+			table[dontCares][ones].add(termList.get(i));
+		}
+		for(int dontKnows=0; dontKnows <= numVars - 1; dontKnows++) {
+			// Use LinkedHashSet for output lists and copy final result to
+			// ArrayList so that we have more efficient operations involving
+			// element identity (contains, removes) while building the list.
+			Set<Term> terms = new LinkedHashSet<Term>(termList);
+			for(int ones=0; ones <= numVars - 1; ones++) {
+				ArrayList<Term> left   = table[dontKnows][ones];
+				ArrayList<Term> right  = table[dontKnows][ones + 1];
+				Set<Term> out    = new LinkedHashSet<Term>();
+				for(int leftIdx = 0; leftIdx < left.size(); leftIdx++) {
+					for(int rightIdx = 0; rightIdx < right.size(); rightIdx++) {
+						Term combined = left.get(leftIdx).combine(right.get(rightIdx));
+						if (combined != null) {
+							if (!out.contains(combined)) {
+								out.add(combined);
+							}
+							terms.remove(left.get(leftIdx));
+							terms.remove(right.get(rightIdx));
+							if (!terms.contains(combined)) {
+								terms.add(combined);
+							}
+						}
+					}
+				}
+				table[dontKnows+1][ones] = new ArrayList<Term>(out);
+			}
+			termList = new ArrayList<Term>(terms);
+		}
 	}
-    }
-    public void reducePrimeImplicantsToSubset() {
-        int numPrimeImplicants = termList.size();
-	int numOriginalTerms   = originalTermList.size();
-	boolean[][] table = new boolean[numPrimeImplicants][numOriginalTerms];
-	for (int impl=0; impl < numPrimeImplicants; impl++) {
-	    for (int term=0; term < numOriginalTerms; term++) {
-	        table[impl][term] = termList.get(impl).implies(originalTermList.get(term));
-	    }
+
+	public void reducePrimeImplicantsToSubset() {
+		int numPrimeImplicants = termList.size();
+		int numOriginalTerms   = originalTermList.size();
+		boolean[][] table = new boolean[numPrimeImplicants][numOriginalTerms];
+		for (int impl=0; impl < numPrimeImplicants; impl++) {
+			for (int term=0; term < numOriginalTerms; term++) {
+				table[impl][term] = termList.get(impl).implies(originalTermList.get(term));
+			}
+		}
+		ArrayList<Term> newTermList = new ArrayList<Term>();
+		boolean done = false;
+		int impl;
+		while (!done) {
+			impl = extractEssentialImplicant(table);
+			if (impl != -1) {
+				newTermList.add(termList.get(impl));
+			} else {
+				impl = extractLargestImplicant(table);
+				if (impl != -1) {
+					newTermList.add(termList.get(impl));
+				} else {
+					done = true;
+				}
+			}
+		}
+		termList = newTermList;
+		originalTermList = null;
 	}
-        ArrayList<Term> newTermList = new ArrayList<Term>();
-	boolean done = false;
-	int impl;
-	while (!done) {
-	    impl = extractEssentialImplicant(table);
-	    if (impl != -1) {
-	        newTermList.add(termList.get(impl));
-	    } else {
-	        impl = extractLargestImplicant(table);
-	        if (impl != -1) {
-	            newTermList.add(termList.get(impl));
-	        } else {
-	            done = true;
-	        }
-	    }
+
+	public static Formula read(Reader reader) throws IOException {
+		ArrayList<Term> terms = new ArrayList<Term>();
+		Term term;
+		while ((term = Term.read(reader)) != null) {
+			terms.add(term);
+		}
+		return new Formula(terms);
 	}
-	termList = newTermList;
-        originalTermList = null;
-    }
-    public static Formula read(Reader reader) throws IOException {
-        ArrayList<Term> terms = new ArrayList<Term>();
-        Term term;
-        while ((term = Term.read(reader)) != null) {
-            terms.add(term);
-        }
-        return new Formula(terms);
-    }
 
-    private int extractEssentialImplicant(boolean[][] table) {
-        for (int term=0; term < table[0].length; term++) {
-            int lastImplFound = -1;
-            for (int impl=0; impl < table.length; impl++) {
-                if (table[impl][term]) {
-                    if (lastImplFound == -1) {
-                        lastImplFound = impl;
-                    } else {
-                        // This term has multiple implications
-                        lastImplFound = -1;
-                        break;
-                    }
-                }
-            }
-            if (lastImplFound != -1) {
-                extractImplicant(table, lastImplFound);
-                return lastImplFound;
-            }
-        }
-        return -1;
-    }
-    private void extractImplicant(boolean[][] table, int impl) {
-        for (int term=0; term < table[0].length; term++) {
-            if (table[impl][term]) {
-                for (int impl2=0; impl2 < table.length; impl2++) {
-                    table[impl2][term] = false;
-                }
-            }
-        }
-    }
-    private int extractLargestImplicant(boolean[][] table) {
-        int maxNumTerms = 0;
-        int maxNumTermsImpl = -1;
-        for (int impl=0; impl < table.length; impl++) {
-            int numTerms = 0;
-            for (int term=0; term < table[0].length; term++) {
-                if (table[impl][term]) {
-                    numTerms++;
-                }
-            }
-            if (numTerms > maxNumTerms) {
-                maxNumTerms = numTerms;
-                maxNumTermsImpl = impl;
-            }
-        }
-        if (maxNumTermsImpl != -1) {
-            extractImplicant(table, maxNumTermsImpl);
-            return maxNumTermsImpl;
-        }
-        return -1;
-    }
+	private int extractEssentialImplicant(boolean[][] table) {
+		for (int term=0; term < table[0].length; term++) {
+			int lastImplFound = -1;
+			for (int impl=0; impl < table.length; impl++) {
+				if (table[impl][term]) {
+					if (lastImplFound == -1) {
+						lastImplFound = impl;
+					} else {
+						// This term has multiple implications
+						lastImplFound = -1;
+						break;
+					}
+				}
+			}
+			if (lastImplFound != -1) {
+				extractImplicant(table, lastImplFound);
+				return lastImplFound;
+			}
+		}
+		return -1;
+	}
+
+	private void extractImplicant(boolean[][] table, int impl) {
+		for (int term=0; term < table[0].length; term++) {
+			if (table[impl][term]) {
+				for (int impl2=0; impl2 < table.length; impl2++) {
+					table[impl2][term] = false;
+				}
+			}
+		}
+	}
+
+	private int extractLargestImplicant(boolean[][] table) {
+		int maxNumTerms = 0;
+		int maxNumTermsImpl = -1;
+		for (int impl=0; impl < table.length; impl++) {
+			int numTerms = 0;
+			for (int term=0; term < table[0].length; term++) {
+				if (table[impl][term]) {
+					numTerms++;
+				}
+			}
+			if (numTerms > maxNumTerms) {
+				maxNumTerms = numTerms;
+				maxNumTermsImpl = impl;
+			}
+		}
+		if (maxNumTermsImpl != -1) {
+			extractImplicant(table, maxNumTermsImpl);
+			return maxNumTermsImpl;
+		}
+		return -1;
+	}
 
 
-    private List<Term> termList;
-    private List<Term> originalTermList;
+	private List<Term> termList;
+	private List<Term> originalTermList;
 
 
-    public static void main(String[] args) throws IOException {
-        Formula f = Formula.read(new BufferedReader(new FileReader(args[0])));
-        f.reduceToPrimeImplicants();
-        f.reducePrimeImplicantsToSubset();
-    }
-    
-    List<Term> getTermList() {
-    	return termList;
-    }
+	public static void main(String[] args) throws IOException {
+		Formula f = Formula.read(new BufferedReader(new FileReader(args[0])));
+		f.reduceToPrimeImplicants();
+		f.reducePrimeImplicantsToSubset();
+	}
+
+	List<Term> getTermList() {
+		return termList;
+	}
 }
--- jaggr-core/src/main/java/com/ibm/jaggr/core/util/Term.java
+++ jaggr-core/src/main/java/com/ibm/jaggr/core/util/Term.java
@@ -31,112 +31,113 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public final class Term {
-    public static final byte DontCare = 2;
+	public static final byte DontCare = 2;
 
-    public Term(byte[] varVals) {
-        this.varVals = varVals;
-        // Terms are immutable, like Strings, so we can calculate the
-        // hash once and save it.
-        this.hash = Arrays.hashCode(varVals);
-    }
+	public Term(byte[] varVals) {
+		this.varVals = varVals;
+		// Terms are immutable, like Strings, so we can calculate the
+		// hash once and save it.
+		this.hash = Arrays.hashCode(varVals);
+	}
 
-    public int getNumVars() {
-        return varVals.length;
-    }
+	public int getNumVars() {
+		return varVals.length;
+	}
 
-    public String toString() {
-        String result = "{"; //$NON-NLS-1$
-        for(int i=0; i<varVals.length; i++) {
-            if (varVals[i] == DontCare)
-                result += "X"; //$NON-NLS-1$
-            else
-                result += varVals[i];
-            result += " "; //$NON-NLS-1$
-        }
-        result += "}"; //$NON-NLS-1$
-        return result;
-    }
+	public String toString() {
+		String result = "{"; //$NON-NLS-1$
+		for(int i=0; i<varVals.length; i++) {
+			if (varVals[i] == DontCare)
+				result += "X"; //$NON-NLS-1$
+			else
+				result += varVals[i];
+			result += " "; //$NON-NLS-1$
+		}
+		result += "}"; //$NON-NLS-1$
+		return result;
+	}
 
-    public Term combine(Term term) {
-        int diffVarNum = -1; // The position where they differ
-        for(int i=0; i<varVals.length; i++) {
-            if (this.varVals[i] != term.varVals[i]) {
-                if (diffVarNum == -1) {
-                    diffVarNum = i;
-                } else {
-                    // They're different in at least two places
-                    return null;
-                }
-            }
-        }
-        if (diffVarNum == -1) {
-            // They're identical
-            return null;
-        }
-        byte[] resultVars = varVals.clone();
-        resultVars[diffVarNum] = DontCare;
-        return new Term(resultVars);
-    }
-    public int countValues(byte value) {
-        int result = 0;
-        for(int i=0; i<varVals.length; i++) {
-            if (varVals[i] == value) {
-                result++;
-            }
-        }
-        return result;
-    }
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (o == null || !getClass().equals(o.getClass()) || hash != ((Term)o).hash) {
-            return false;
-        }
-        return Arrays.equals(this.varVals, ((Term)o).varVals);
-    }
+	public Term combine(Term term) {
+		int diffVarNum = -1; // The position where they differ
+		for(int i=0; i<varVals.length; i++) {
+			if (this.varVals[i] != term.varVals[i]) {
+				if (diffVarNum == -1) {
+					diffVarNum = i;
+				} else {
+					// They're different in at least two places
+					return null;
+				}
+			}
+		}
+		if (diffVarNum == -1) {
+			// They're identical
+			return null;
+		}
+		byte[] resultVars = varVals.clone();
+		resultVars[diffVarNum] = DontCare;
+		return new Term(resultVars);
+	}
+	public int countValues(byte value) {
+		int result = 0;
+		for(int i=0; i<varVals.length; i++) {
+			if (varVals[i] == value) {
+				result++;
+			}
+		}
+		return result;
+	}
 
-    public int hashCode() {
-        return hash;
-    }
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (o == null || !getClass().equals(o.getClass()) || hash != ((Term)o).hash) {
+			return false;
+		}
+		return Arrays.equals(this.varVals, ((Term)o).varVals);
+	}
 
-    boolean implies(Term term) {
-        for(int i=0; i<varVals.length; i++) {
-            if (this.varVals[i] != DontCare &&
-                this.varVals[i] != term.varVals[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public int hashCode() {
+		return hash;
+	}
 
-    byte get(int i) {
-    	return varVals[i];
-    }
+	boolean implies(Term term) {
+		for(int i=0; i<varVals.length; i++) {
+			if (this.varVals[i] != DontCare &&
+					this.varVals[i] != term.varVals[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public static Term read(Reader reader) throws IOException {
-        int c = '\0';
-        ArrayList<Byte> t = new ArrayList<Byte>();
-        while (c != '\n' && c != -1) {
-            c = reader.read();
-            if (c == '0') {
-                t.add((byte)0);
-            } else if (c == '1') {
-                t.add((byte)1);
-            }
-        }
-        if (t.size() > 0) {
-            byte[] resultBytes = new byte[t.size()];
-            for(int i=0; i<t.size(); i++) {
-                resultBytes[i] = (byte)t.get(i);
-            }
-            return new Term(resultBytes);
-        } else {
-            return null;
-        }
-    }
+	byte get(int i) {
+		return varVals[i];
+	}
 
-    private final byte[] varVals;
-    private final int hash;
+	public static Term read(Reader reader) throws IOException {
+		int c = '\0';
+		ArrayList<Byte> t = new ArrayList<Byte>();
+		while (c != '\n' && c != -1) {
+			c = reader.read();
+			if (c == '0') {
+				t.add((byte)0);
+			} else if (c == '1') {
+				t.add((byte)1);
+			}
+		}
+		if (t.size() > 0) {
+			byte[] resultBytes = new byte[t.size()];
+			for(int i=0; i<t.size(); i++) {
+				resultBytes[i] = (byte)t.get(i);
+			}
+			return new Term(resultBytes);
+		} else {
+			return null;
+		}
+	}
+
+	private final byte[] varVals;
+	private final int hash;
 }
 
