df9acc6778444ffddde9880ca6f6841df0dc6bb7
chuckd <chuckd@us.ibm.com>
2015-02-17 13:22:54 -0500

    Fix potential reference error in js code.

--- jaggr-core/WebContent/dojo/_loaderExt.js
+++ jaggr-core/WebContent/dojo/_loaderExt.js
@@ -145,7 +145,7 @@ combo.done = function(load, config, opt_deps) {
 			
 			var url = contextPath;
 			url = addModulesToUrl(url, ["modules", "moduleIds"], opt_deps, moduleIdMap, base64 ? base64.encode : null);
-			url = addModulesToUrl(url, ["reqExpEx", "reqExpExIds"], combo.bootLayerDeps, moduleIdMap,  base64 ? base64.encode : null);
+			url = addModulesToUrl(url, ["reqExpEx", "reqExpExIds"], combo.bootLayerDeps || [], moduleIdMap,  base64 ? base64.encode : null);
 			url += (hasArg ? '&' + hasArg : "");
 			
 			// Allow any externally provided URL processors to make their contribution
