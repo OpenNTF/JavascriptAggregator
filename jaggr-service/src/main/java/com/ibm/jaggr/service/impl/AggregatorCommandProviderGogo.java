854307b5c451788db5476d8f34b620c0d4fdedeb
Dan Dumont <ddumont@us.ibm.com>
2016-08-05 11:44:02 -0400

    Update copyrights.

--- jaggr-service/src/main/java/com/ibm/jaggr/service/impl/AggregatorCommandProviderGogo.java
+++ jaggr-service/src/main/java/com/ibm/jaggr/service/impl/AggregatorCommandProviderGogo.java
@@ -1,5 +1,5 @@
 /*
-help * (C) Copyright IBM Corp. 2012, 2016
+help * (C) Copyright IBM Corp. 2012, 2016 All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
